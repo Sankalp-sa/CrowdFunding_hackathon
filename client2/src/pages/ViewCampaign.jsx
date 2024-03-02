05871817e74c1240e1ff582a2eb902dd46e1f825
Parv Patel <101592578+parvpatel2609@users.noreply.github.com>
2024-03-03 02:46:15 +0530

    updated view

--- client2/src/pages/ViewCampaign.jsx
+++ client2/src/pages/ViewCampaign.jsx
@@ -5,31 +5,40 @@ import Navbar from '../components/Navbar';
 
 export default function ViewCampaign() {
 
-  const [campaigns, setCampaigns] = useState([]);
+    const [campaigns, setCampaigns] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await axios.get("http://localhost:5000/campaign/");
-      setCampaigns(response.data);
-      console.log(response.data);
+    useEffect(() => {
+        async function fetchData() {
+            const response = await axios.get("http://localhost:5000/campaign/");
+            setCampaigns(response.data);
+            console.log(response.data);
+        }
+
+        fetchData();
     }
+        , []);
 
-    fetchData();
-  }
-  , []);
+    return (
+        <div>
 
-  return (
-    <div>
+            <Navbar />
 
-      <Navbar />
-
-      <section id="gallery">
-          <div className="container">
-              <div className="row">
-                  
-              </div>
-          </div>
-      </section>
-  </div>
-  )
+            <section id="gallery">
+                <div className="container">
+                    <div className="row">
+                        {campaigns.map((campaign, index) => (
+                            <div key={index}>
+                                <h2>{campaign.title}</h2>
+                                <p>Owner: {campaign.owner}</p>
+                                <p>Description: {campaign.description}</p>
+                                <p>Target: {campaign.target}</p>
+                                <p>Deadline: {new Date(campaign.deadline * 1000).toLocaleDateString()}</p>
+                                <p>Amount Collected: {campaign.amountCollected}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 }
